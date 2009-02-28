be4e608d687d1345372e9db6f8193970eb4bfb5b
pablobm <pablobm@gmail.com>
2009-02-28 15:10:41 +0000

    So it seems I had not added the JSR135Support class to the repository, oh well... Also populated the .gitignore file with appropriate content.

---
+++ src/com/pablobm/jwhat/profiles/JSR135Support.java
@@ -0,0 +1,49 @@
+package com.pablobm.jwhat.profiles;
+
+import java.util.*;
+
+public class JSR135Support
+	extends BaseProfileSupport
+{
+	public JSR135Support() {
+		name = "JSR-135";
+		version = System.getProperty("microedition.media.version");
+		longName = "Mobile Media API";
+		description = "Handling of media content and protocols";
+		summary = composeSummary();
+	}
+	
+	private String composeSummary() {
+		Vector supports = new Vector();
+		
+		if (boolProp("supports.mixing")) {
+			supports.addElement("mixing");
+		}
+		if (boolProp("supports.audio.capture")) {
+			supports.addElement("audio capture");
+		}
+		if (boolProp("supports.video.capture")) {
+			supports.addElement("video capture");
+		}
+		if (boolProp("supports.recording")) {
+			supports.addElement("recording");
+		}
+		
+		return vectorToString(supports);
+	}
+	
+	private String vectorToString(Vector v) {
+		if (v.size() == 0) {
+			return "no support";
+		}
+
+		Enumeration strings = v.elements();
+		String ret = (String)strings.nextElement();
+		while (strings.hasMoreElements()) {
+			ret += ", " + strings.nextElement();
+		}
+		
+		return ret;		
+	}
+}
+
